32812a0aa673d4d2ad7a6aed9f804641a7aa7f3d
Emmanuel Egomson <fegomson@gmail.com>
2025-02-25 11:31:16 -0600

    Initial setup with tailwind for styling

---
+++ frontend/src/App.tsx
@@ -0,0 +1,6 @@
+const App = () => {
+  return (
+    <h1 className="text-4xl font-bold">App</h1>
+  )
+}
+export default App
---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as Home } from "./Dashboard/Home";
+export { default as Income } from "./Dashboard/Income";
+export { default as Expense } from "./Dashboard/Expense";
+export { default as Login } from "./Auth/Login";
+export { default as SignUp } from "./Auth/SignUp";
